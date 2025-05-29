3ffb346278f5b005fc87ec0b9462e6df551c10d6
simha97 <62260118+simha97@users.noreply.github.com>
2025-05-29 19:32:35 +0200

    link

--- backend/main.py
+++ backend/main.py
@@ -21,7 +21,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://ask-the-emperor.vercel.app/",        
+        "https://ask-the-emperor.vercel.app",        
         "http://localhost:5173",
         "http://127.0.0.1:5173"],  
     allow_credentials=True,
